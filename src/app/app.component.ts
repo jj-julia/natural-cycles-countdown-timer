ee29f4f5efc836810bdda640b5973ab6a3aaf1e5
“Julia <wells.julia@gmail.com>
2025-02-06 12:57:09 +0100

    Setup incl. global styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
+import { FormsModule } from '@angular/forms';
+import { EventInputComponent } from './components/event-input/event-input.component';
+import { CountdownDisplayComponent } from './components/countdown-display/countdown-display.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'FrontendChallenge'
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router'
+import { CountdownPageComponent } from './pages/countdown-page/countdown-page.component'
 
-export const routes: Routes = []
+export const routes: Routes = [
+  { path: '', component: CountdownPageComponent },
+]
